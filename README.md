042ccabe917cbebcdc23d844e1362cf0fd7b7334
everruler12 <erik.newhard@gmail.com>
2021-01-31 20:49:40 -0700

    EDN support

--- README.md
+++ README.md
@@ -2,17 +2,42 @@
 
 Inspired by https://github.com/MatthieuBizien/roam-to-git
 
+roam-to-git has offered a great relief for my mind. However, it was not uncommon for backups to fail with unknown errors a couple times a day. Then it got to the point where all my backups were failing. The creator is not very active with support, and I don't know Python to fork and edit. So I decided to roll my own backup solution using Node, with clearer logging to make troubleshooting easier.
+
 ### Differences
 
 - Uses Node (rather than Python).
-- Only backups up JSON (markdown causes too many errors)
+- Backups JSON and EDN (Skips markdown for now, as it has caused too many errors and timeouts)
 - better error debugging
 
-### Demo with instruction how to use
+### Instructions
+
+Assuming you're coming from roam-to-git and have followed my [guide](https://eriknewhard.com/blog/backup-roam-in-github), you can find simple instructions on how to update your Secrets and main.yml here:
 
 https://github.com/everruler12/roam2github-demo
 
 ### Future Plans
 
-- [ ] edn support
+- [x] edn support
+- [ ] new, full guide for non-roam-to-git users
 - [ ] multiple graph backups in same repo
+- [ ] markdown support
+- [ ] any other ideas? Let me know!
+
+### EDN Backups are live!
+
+The backup has a check to make sure the formatted EDN (which only adds extra linebreaks and indentation) can be parsed back to match exactly with the original before saving it. It will exit with an error if it can't, so you can rest assured that the formatting doesn't mess with the file integrity. I also tested that the formatted EDN can be used to successfully restore graphs.
+
+2021-01-31 It took all day to figure out how to use ClojureScript to prettify EDN. It was a daunting task, never having dealt with Clojure before, much less compiling it into JavaScript. But I did it! This is necessary because the exported EDN data from Roam is all in one line, meaning GitHub would have to save the entire file each time, instead of just the new lines. This would eat up the storage pretty quickly if run every hour, as unchanged notes would be duplicated each time. And you wouldn't be able to see line-by-line changes in the git history.
+
+### Support / Donations
+
+If you experience any issues or errors with my backup script, let me know! Either post as a GitHub issue here, or send me a message at my support email [erik@eriknewhard.com](mailto:erik@eriknewhard.com)
+
+---
+
+Some very generous people have been asking how to donate. If you like my work, I won't refuse your support!
+
+Email for PayPal and Amazon gift cards: [erik.newhard@gmail.com](erik.newhard@gmail.com)
+
+Bitcoin (BTC) address: bc1qsa3l8lraa3rjj6wyc7zdlv5z2xnlunppavtxw0
--- roam2github.js
+++ roam2github.js
@@ -1,11 +1,13 @@
 const path = require('path')
-const fs = require('fs-extra') // for mkdirp() and output() and remove() ~~and move()~~ and to promisfy all so don't have to use fs.promises
+const fs = require('fs-extra')
 const puppeteer = require('puppeteer')
 const extract = require('extract-zip')
 
+const edn_formatter = require('./edn_formatter.js').edn_formatter.core
+
 console.time('R2G Exit after')
 
-// NEED better check, because .env could exist in repo. like check of secrets exist in process.env, if so, IS_GITHUB_ACTION = true, other wise try local .env, and check again
+// NEED better check, because .env could exist in repo. like check if secrets exist in process.env, if so, IS_GITHUB_ACTION = true, other wise try local .env, and check again
 let IS_LOCAL
 
 try {
@@ -19,7 +21,10 @@ try {
 } catch (err) { error(`.env file existence error: ${err}`) }
 
 const download_dir = path.join(__dirname, 'tmp')
+const extract_dir = path.join(download_dir, '_extraction')
 const backup_dir = IS_LOCAL ? path.join(__dirname, 'backup') : getRepoPath()
+
+let downloads_started = 0
 
 const { R2G_EMAIL, R2G_PASSWORD, R2G_GRAPH } = process.env
 
@@ -38,7 +43,7 @@ init()
 
 async function init() {
     try {
-        // deleteDir(download_dir)
+        await fs.remove(download_dir, { recursive: true })
 
         log('Creating browser')
         const browser = await puppeteer.launch({ args: ['--no-sandbox'] }) // to run in GitHub Actions
@@ -47,14 +52,22 @@ async function init() {
         const page = await browser.newPage()
         page.setDefaultTimeout(600000) // 10min
         await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: download_dir })
+        // page.on('console', consoleObj => console.log(consoleObj.text())) // for console.log() to work in page.evaluate() https://stackoverflow.com/a/46245945
+        // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36'); // https://github.com/puppeteer/puppeteer/issues/1477#issuecomment-437568281
 
         await roam_login(page)
-        await roam_export(page)
+
+        // for each graph {
+        await roam_open_graph(page)
+        await roam_download(page, 'JSON')
+        await roam_download(page, 'EDN')
+        // }
 
         log('Closing browser')
         browser.close()
 
-        await extract_json()
+        await extract_zips()
+        await format_and_save()
         // deleteDir(download_dir)
 
     } catch (err) { error(err) }
@@ -74,6 +87,7 @@ async function roam_login(page) {
             log('Signing in')
             // log('Waiting for login form')
             await page.waitForSelector(email_selector)
+            // possible refresh a second time on login screen https://github.com/MatthieuBizien/roam-to-git/issues/87#issuecomment-763281895
 
             // log('Filling email field')
             await page.type(email_selector, R2G_EMAIL)
@@ -96,7 +110,7 @@ async function roam_login(page) {
                 const error_message = await page.evaluate(el => el.innerText, error_el)
                 reject(`Login error. Roam says: "${error_message}"`)
             } else if (await page.$(graphs_selector)) {
-                log('Login successful')
+                log('Login successful!')
                 resolve()
             } else { // timeout?
                 reject('Login error: unknown')
@@ -106,13 +120,12 @@ async function roam_login(page) {
     })
 }
 
-async function roam_export(page) {
+async function roam_open_graph(page) {
     return new Promise(async (resolve, reject) => {
         try {
 
-            // TODO allow multiple graphs
-
             log('Navigating to graph')
+            // log('Navigating to graph', R2G_GRAPH.split('').join(' '))
             await page.goto('https://roamresearch.com/404')// workaround to get disablecss and disablejs parameters to work by navigating away due to issue with puppeteer and # hash navigation (used in SPAs like Roam)
             await page.goto(`https://roamresearch.com/#/app/${R2G_GRAPH}?disablecss=true&disablejs=true`)
 
@@ -128,7 +141,16 @@ async function roam_export(page) {
             //     await page.waitForSelector('.navbar') // Likely screen saying 'You do not have permission to view this database'
             //     reject()
             // }
-            log('Graph loaded')
+            log('Graph loaded!')
+            resolve()
+
+        } catch (err) { reject(err) }
+    })
+}
+
+async function roam_download(page, filetype) {
+    return new Promise(async (resolve, reject) => {
+        try {
 
             await page.waitForSelector('.bp3-icon-more')
 
@@ -141,102 +163,141 @@ async function roam_export(page) {
             })
 
             // log('Waiting for export dialog')
-            await page.waitForSelector('.bp3-dialog .bp3-button-text')
+            const chosen_format_selector = '.bp3-dialog .bp3-button-text'
+            await page.waitForSelector(chosen_format_selector)
 
-            // log('Clicking Export Format')
-            await page.click('.bp3-dialog .bp3-button-text')
+            const chosen_format = await page.$eval(chosen_format_selector, el => el.innerText)
 
+            if (filetype != chosen_format) {
+                // log('Clicking Export Format')
+                await page.click(chosen_format_selector)
 
-            // log('Clicking "JSON"')
-            await page.evaluate(() => {
-                [...document.querySelectorAll('.bp3-text-overflow-ellipsis')].find(dropdown => dropdown.innerText == 'JSON').click()
-            })
+                page.waitForSelector('.bp3-text-overflow-ellipsis')
+
+                // log('Choosing', filetype)
+                await page.evaluate((filetype) => {
+                    [...document.querySelectorAll('.bp3-text-overflow-ellipsis')].find(dropdown => dropdown.innerText == filetype).click()
+                    // [...document.querySelectorAll('.bp3-text-overflow-ellipsis')].find(dropdown => dropdown.innerText == 'JSON').click()
+                }, filetype)
+
+            } else {
+                // log(filetype, 'already selected')
+            }
 
             // log('Clicking "Export All"')
             await page.evaluate(() => {
                 [...document.querySelectorAll('button')].find(button => button.innerText == 'Export All').click()
             })
 
-            log('Waiting for JSON download to start')
+            log('Waiting for', filetype, 'download to start')
             await page.waitForSelector('.bp3-spinner')
             await page.waitForSelector('.bp3-spinner', { hidden: true })
 
-            log('Downloading JSON')
+            // log('Downloading', filetype)
+            downloads_started++
+
             const checkDownloads = async () => {
                 const files = await fs.readdir(download_dir)
 
-                if (files[0] && files[0].match(/\.zip$/)) { // contains .zip file
-                    log('JSON container downloaded:', files[0])
+                if (files && files.filter(file => file.match(/\.zip$/)).length == downloads_started) { // contains .zip file
+                    log('Downloaded', filetype)
                     resolve()
                 } else checkDownloads()
             }
             checkDownloads()
-            // TODO how to check multiple downloads?
 
         } catch (err) { reject(err) }
     })
 }
 
-async function extract_json() {
+async function extract_zips() {
     return new Promise(async (resolve, reject) => {
         try {
 
-            // log('Checking download_dir')
             const files = await fs.readdir(download_dir)
 
-            if (files.length === 0) {
-                reject('Extraction error: download dir is empty')
+            if (files.length === 0) reject('Extraction error: download_dir is empty')
 
-            } else if (files) {
-                // log('Found', files)
-                const file = files[0]
+            for (const file of files) {
+                log('Extracting ' + file)
+                const file_fullpath = path.join(download_dir, file)
 
-                const source = path.join(download_dir, file)
-                const target = path.join(download_dir, '_extraction')
-
-                log('Extracting JSON from ' + file)
-                await extract(source, { dir: target })
+                await extract(file_fullpath, { dir: extract_dir })
                 // log('Extraction complete')
-
-                const json_filename = `${R2G_GRAPH}.json`
-                const json_fullpath = path.join(target, json_filename)
-                const new_json_fullpath = path.join(backup_dir, 'json', json_filename)
-
-                log('Formatting JSON')
-                const json = await fs.readJson(json_fullpath)
-                const new_json = JSON.stringify(json, null, 2)
-
-                // log('Saving formatted JSON')
-                await fs.outputFile(new_json_fullpath, new_json)
-
-                log('Cleaning up')
-                // log('Deleting download_dir')
-                await fs.remove(download_dir, { recursive: true })
-                // log('download_dir deleted')
-
-                resolve()
             }
+
+            resolve()
 
         } catch (err) { reject(err) }
     })
 }
 
-// async function deleteDownloads(dir) {
-//     // if already doesn't exist, don't log
-//     fs.rmdir(download_dir, { recursive: true })
-//     log('download dir deleted')
-// }
+
+async function format_and_save() {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            const files = await fs.readdir(extract_dir)
+
+            if (files.length === 0) reject('Extraction error: extract_dir is empty')
+
+            for (const file of files) {
+                const file_fullpath = path.join(extract_dir, file)
+                const fileext = file.split('.').pop()
+                const new_file_fullpath = path.join(backup_dir, fileext, file)
+
+                if (fileext == 'json') {
+
+                    // log('Formatting JSON')
+                    const json = await fs.readJson(file_fullpath)
+                    const new_json = JSON.stringify(json, null, 2)
+
+                    log('Saving formatted JSON')
+                    fs.outputFile(new_file_fullpath, new_json)
+                } else if (fileext == 'edn') {
+                    const edn_prefix = '#datascript/DB '
+
+                    log('Formatting EDN') // This could take awhile for large graphs
+                    const edn = await fs.readFile(file_fullpath, 'utf-8')
+                    var new_edn = edn_prefix + edn_formatter.format(edn.replace(edn_prefix, ''))
+                    checkFormattedEDN(edn, new_edn)
+
+                    log('Saving formatted EDN')
+                    fs.outputFile(new_file_fullpath, new_edn)
+
+                }
+            }
+
+            resolve()
+
+        } catch (err) { reject(err) }
+    })
+}
 
 function log(...messages) {
     const timestamp = new Date().toISOString().replace('T', ' ').replace('Z', '')
     console.log(timestamp, 'R2G', ...messages)
 }
 
-function error(err) {
+async function error(err) {
     log('ERROR -', err)
     console.timeEnd('R2G Exit after')
+    // await page.screenshot({ path: path.join(download_dir, 'error.png' }) // will need to pass page as parameter... or set as parent scope
     process.exit(1)
 }
 
-// IDEA commit screenshot if error instead of process.exit(1)
-// await page.screenshot({path: `error ${timestamp}.png`}) // will need to pass page as parameter... or set as parent scope
+function checkFormattedEDN(original, formatted) {
+    const reverse_format = formatted
+        .trim() // remove trailing line break
+        .split('\n') // separate by line
+        .map(line => line.trim()) // remove indents, and one extra space at end of second to last line
+        .join(' ') // replace line breaks with a space
+
+    if (original === reverse_format) {
+        // log('(formatted EDN check successful)') // formatted EDN successfully reversed to match exactly with original EDN
+        return true
+    } else {
+        error('EDN formatting error: mismatch with original')
+        return false
+    }
+}
